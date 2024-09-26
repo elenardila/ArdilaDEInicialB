2a42b5b31b18b173d8aa9b8832836ef1a2eb841f
ElenaArdila <eardilad01@iesalbarregas.es>
2024-09-26 16:08:23 +0200

    Actualización clase 25/09/24
    Creación de un nuevo Servlet llamado Datos y ampliación de campos del formulario.

---
+++ src/main/java/es/albarregas/controllers/Datos.java
@@ -0,0 +1,88 @@
+package es.albarregas.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author elena
+ */
+@WebServlet(name = "Controlador", urlPatterns = {"/Controlador"})
+public class Datos extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Datos</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Datos at " + request.getContextPath() + "</h1>");
+            out.println("<h2>Llamda realizada por el m&eacute;todo " + request.getMethod() + "</h2>");
+            Enumeration<String> parametros = request.getParameterNames();
+            if (parametros.hasMoreElements()) {
+                while (parametros.hasMoreElements()) {
+                    String nombre = parametros.nextElement();
+                    out.println("<p>El valor de " + nombre + " es " + request.getParameter(nombre) + "</p>");
+                }
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
